4fd36352265a131b0bb9c70599f24544cef1d855
Kolja Wawrowsky <3075215+apocryphx@users.noreply.github.com>
2026-05-09 15:16:28 -0700

    Initial Commit

---
+++ GCDWebServer/GCDWebServer.h
@@ -0,0 +1,16 @@
+//
+//  GCDWebServer.h
+//  GCDWebServer
+//
+//  Created by Kolja Wawrowsky on 5/9/26.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GCDWebServer.
+FOUNDATION_EXPORT double GCDWebServerVersionNumber;
+
+//! Project version string for GCDWebServer.
+FOUNDATION_EXPORT const unsigned char GCDWebServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GCDWebServer/PublicHeader.h>
